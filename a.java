439be20c329e41c8901d67de9b9b28c6ee2a3b85
Heejong Park <hpar081@aucklanduni.ac.nz>
2014-10-10 14:58:38 +1300

    MM

---
+++ a.java
@@ -0,0 +1,12 @@
+package a;
+import systemj.lib.Signal;
+public class a {
+	public static Signal a;
+	public static void main (String [] args)
+	{
+		a = new Signal();
+		Integer www = new Integer(123);
+		a.setValue(www);
+	}
+
+}
